4bd9630f5835adfe6dec6f9b19e671443d382f00
pvol <pvol@163.com>
2015-12-08 11:39:53 +0800

    添加手工指定操作用户的支持

--- src/Util/User.php
+++ src/Util/User.php
@@ -2,11 +2,32 @@
 
 namespace Pvol\FlowMatrix\Util;
 
-use Auth;
+use Auth,Session;
 
 class User {
     
     public static function info(){
-        return Auth::user();
+        // 取登录帐号
+        $user = Auth::user();
+        if (!empty($user)) {
+            return $user;
+        }
+        // 指定用户
+        $user = self::getUser();
+        if (!empty($user)) {
+            return $user;
+        }
+        return false;
+    }
+    
+    /**
+     * 设置用户(第三方开放接口使用)
+     */
+    public static function setUser($user) {
+        return Session::set('flow_matrix_user', $user);
+    }
+    
+    public static function getUser() {
+        return Session::get('flow_matrix_user');
     }
 }
